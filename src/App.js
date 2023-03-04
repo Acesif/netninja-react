574e1dedcf2e2641d3466b456ad3a6f8aa6b6801
Acesif <asifpalak@gmail.com>
2023-03-05 04:25:50 +0600

    3/5/23

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import './App.css';
 import React from 'react'
 
 function App() {
-  let [item,setItem] = React.useState([]);
-  const clickHandler = () =>{
-    setItem([
+  let [show,setShow] = React.useState(false);
+  let [item,setItem] = React.useState([
     {title:"green", id:1},
     {title:"demonic", id:2},
     {title:"thunder", id:3},
     {title:"ice", id:4}
-    ])
-  }
+    ]);
   const removeItem = (id) =>{
     return setItem((prevItem)=>
       prevItem.filter((i) => {
@@ -20,8 +18,8 @@ function App() {
   return (
     <div className="App">
       <h1>Item List</h1>
-      <button onClick={clickHandler}>Click Me</button>
-      {item.map((e) =>(
+      <button onClick={()=>setShow(!show)}>Toggle</button>
+      {show && item.map((e) =>(
         <div key={e.id}>
           <li>{e.title}</li>
           <button onClick={() => removeItem(e.id)}>Delete</button>
