d4e146aca4ecf71db8b6175dd45e4b8a126c922c
Acesif <asifpalak@gmail.com>
2023-03-10 02:09:45 +0600

    3/10/23

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Title from './components/Title'
 import Modal from './components/Modal'
 
 export default function App() {
-  let [modal, changeModal] = React.useState(true);
+  let [modal, changeModal] = React.useState(false);
   let [show,setShow] = React.useState(false);
   let [item,setItem] = React.useState([
     {title:"green", id:1},
@@ -22,6 +22,7 @@ export default function App() {
     <div className="App">
       <Title title="Listings" subtitle="Delete unnecessary items" />
       <button className='btn' onClick={()=>setShow(!show)}>Toggle</button>
+      <button className='btn' onClick={()=> changeModal(true)}>Show Modal</button>
       {show && item.map((e) =>(
         <div className='box' key={e.id}>
           <li>{e.title}</li>
